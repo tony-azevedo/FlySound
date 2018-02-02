32910b33de39c812b8878fecc2b35b3728c0b4c4
Anthony Azevedo <tazevedo@gmail.com>
2018-02-02 13:29:46 -0800

    Beginning new basler camera setup

---
+++ FlySoundDevices/CameraBasler.m
@@ -0,0 +1,159 @@
+classdef CameraBasler < Device
+    
+    properties (Constant)
+        
+    end
+    
+    properties (Hidden, SetAccess = protected)
+    end
+    
+    properties (SetAccess = protected)
+        gaincorrection
+        videoInput
+        source
+        fileDestination
+        fileName
+        living
+        cameraLocation
+        format
+        camPortID
+        triggermode
+        triggerpolarity
+        dispFunc
+    end
+    
+    properties
+    end
+
+    events
+        %
+    end
+    
+    methods
+        function obj = CameraBasler(varargin)
+            % This and the transformInputs function are hard coded
+            
+            obj.gaincorrection = [];
+            obj.videoInput = [];
+            obj.source = [];
+            obj.fileDestination = 'C:\Users\tony\Acquisition\fly_movement_videos';
+            obj.fileName = 'default_name';
+            obj.living = 0;
+            obj.cameraLocation = 'PGRCamDefaultLoc';
+            obj.format = 'Mono8';
+            obj.camPortID = 1;
+            obj.triggermode = '';
+            obj.triggerpolarity = 'risingEdge';
+        end
+        
+        function setup(obj,varargin)
+            % See acquire_fly_movement_01_2018
+
+            fprintf('Turn off Pylon software');
+            h = warndlg('Turn off Pylon software');
+            uiwait(h)
+            % imaqreset
+            fprintf('\n')
+
+            obj.videoInput = videoinput('gentl', obj.camPortID, obj.format); %vid = videoinput('gentl', 1, 'Mono8');
+            obj.source = getselectedsource(obj.videoInput);
+            
+            % Setup source and pulses etc
+            triggerconfig(obj.videoInput, 'hardware');
+            obj.videoInput.TriggerRepeat = 0;
+            obj.videoInput.FramesPerTrigger = 1;
+            obj.videoInput.LoggingMode = 'disk&memory';
+            
+            
+            obj.living = 0;
+
+        end
+        
+        function varargout = transformInputs(obj,inputstruct,varargin)
+            inlabels = fieldnames(inputstruct);
+            units = {};
+            for il = 1:length(inlabels)
+                if strcmp(inlabels{il},'exposure')
+                    units = {'bit'};
+                   
+                    inputstruct.exposure = inputstruct.exposure > 0.5;
+                    inputstruct.exposure = ...
+                        [inputstruct.exposure(2:end) - inputstruct.exposure(1:end-1); 0];
+                    inputstruct.exposure = inputstruct.exposure > 0;
+                end
+            end
+            varargout = {inputstruct,units};
+        end
+        
+        function out = transformOutputs(obj,out,varargin)
+            rig = varargin{1};
+            if ~isfield(out,'trigger')
+                fns = fieldnames(out);
+                out.trigger = out.(fns{1});
+                out.trigger(:) = 0;
+            else
+            end
+            dur = length(out.trigger)/rig.aoSession.Rate;
+            frametime = 1/obj.params.framerate;
+            Nframes = floor(dur/frametime);
+            idxs = round(((1:Nframes)-1)*(frametime*rig.aoSession.Rate) + 1);
+            out.trigger(idxs) = 1;
+            out.trigger(idxs+1) = 0;
+            out.trigger(idxs(2)) = 0;
+            out.trigger(idxs(2)+1) = 0;
+            out.trigger(idxs(3)) = 0;
+            out.trigger(idxs(3)+1) = 0;
+            out.trigger(end) = 0;
+            obj.params.Nframes = sum(out.trigger)/1;%/2;
+            %out.trigger = ~out.trigger;
+            obj.params.Nframes = sum(out.trigger)/2;
+            obj.videoInput.TriggerRepeat = obj.params.Nframes-1;
+            obj.videoInput.TriggerFcn = {obj.dispFunc};
+        end
+    
+        function live(obj)
+            preview(obj.videoInput)
+            obj.living = 1;
+        end
+        function dead(obj)
+            stoppreview(obj.videoInput)
+            obj.living = 0;
+
+        end
+        function start(obj)
+            start(obj.videoInput);
+            fprintf('Video started: %d of %d Triggers/Frames\n\tFilename:\t%s\n\tFilepath:\t%s\n',...
+                obj.videoInput.FramesAcquired,obj.videoInput.TriggerRepeat+1,get(obj.videoInput.DiskLogger,'Filename'),get(obj.videoInput.DiskLogger,'Path'))
+        end
+        function stop(obj)
+            fprintf('Triggers fired: %d of %d. Logger logged %d frames.\n',obj.videoInput.TriggersExecuted,obj.videoInput.TriggerRepeat+1,obj.videoInput.DiskLoggerFrameCount)
+            stop(obj.videoInput)
+        end
+        
+        function varargout = status(obj)
+            s = sprintf('Triggers fired: %d of %d. Logger logged %d frames.\n',obj.videoInput.TriggersExecuted,obj.videoInput.TriggerRepeat+1,obj.videoInput.DiskLoggerFrameCount);
+            missedFrames = obj.videoInput.TriggerRepeat+1 - obj.videoInput.TriggersExecuted;
+            varargout = {obj.videoInput.Running,s,missedFrames};
+        end
+        
+        function setLogging(obj,filename)
+            [fn,D] = strtok(fliplr(filename),filesep);
+            obj.fileName = fliplr(fn);
+            obj.fileDestination = fliplr(D(2:end));
+            diskLogger = VideoWriter([fullfile(obj.fileDestination,obj.fileName) '.avi'], 'Motion JPEG AVI');
+            obj.videoInput.DiskLogger = diskLogger;
+        end
+        
+    end
+    
+    methods (Access = protected)
+        function setupDevice(obj)        
+        end
+
+        function defineParameters(obj)
+            obj.params.setup = 'x Frames, write in the rest of the information';
+            obj.params.framerate = 40;
+            obj.params.Nframes = 30;
+        end
+    end
+end
---
+++ FlySoundRigs/CameraBaslerRig.m
@@ -0,0 +1,90 @@
+classdef CameraBaslerRig < EPhysRig
+    % current hierarchy: 7/14/16
+    %   Rig -> EPhysRig -> BasicEPhysRig
+    %                   -> TwoTrodeRig
+    %                   -> PiezoRig 
+    %                   -> TwoPhotonRig -> TwoPhotonEPhysRig 
+    %                                   -> TwoPhotonPiezoRig     
+    %                   -> CameraRig    -> CameraEPhysRig 
+    %                                   -> PiezoCameraRig 
+    %                   -> PGRCameraRig -> PGREPhysRig
+    %                                   -> PGRPiezoRig % This setup is for
+    %                                   a digital output that requires same
+    %                                   session, and same input and output
+    %                                   sample rates
+    %                   -> BasicEPhysRigSS
+    
+    properties (Constant,Abstract)
+        rigName;
+        IsContinuous;
+    end
+    
+    methods
+        function obj = CameraBaslerRig(varargin)
+            obj.addDevice('camera',CameraBasler);
+            addlistener(obj,'StartTrial',@obj.readyCamera);
+            addlistener(obj,'DataSaved',@obj.resetCamera);
+        end
+        
+
+        
+        function readyCamera(obj,fig,evnt,varargin)
+            obj.devices.camera.start()
+            
+        end
+        
+        function resetCamera(obj,fig,evnt,varargin)
+            obj.devices.camera.stop()
+            
+        end
+
+        function setDisplay(obj,fig,evnt,varargin)
+            setDisplay@Rig(obj,fig,evnt,varargin{:})
+            camfig = figure(1001); clf;
+            set(camfig,'position',[1120 31 560 420],'name','PGR Camera','tag','PGRCamFig')
+            camax = axes('parent',camfig,'units','normalized','position',[0 0 1 1],'tag','PGRCamAx');
+            set(camax,'box','on','xtick',[],'ytick',[]);
+            if nargin>3
+                protocol = varargin{1};            
+                ax = subplot(3,1,[1 2],'Parent',obj.TrialDisplay,'tag','inputax');
+                delete(findobj(ax,'tag','ampinput'));
+                line(makeTime(protocol),makeTime(protocol),'parent',ax,'color',[1 0 0],'linewidth',1,'tag','ampinput','displayname','input');
+                ylabel('Amp Input'); box off; set(gca,'TickDir','out');
+                
+                ax = subplot(3,1,3,'Parent',obj.TrialDisplay,'tag','outputax');
+                
+                line(makeInTime(protocol),makeInTime(protocol),'parent',ax,'color',[0 0 1],'linewidth',1,'tag','exposure','displayname','V');
+                ylabel('SGS (V)'); box off; set(gca,'TickDir','out');
+                xlabel('Time (s)'); %xlim([0 max(t)]);
+                linkaxes(get(obj.TrialDisplay,'children'),'x');
+            end
+        end
+        
+        function displayTrial(obj,protocol)
+            if ~ishghandle(obj.TrialDisplay), obj.setDisplay(protocol), end
+
+            if strcmp(obj.devices.amplifier.mode,'VClamp')
+                invec = obj.inputs.data.current;
+                ind = find(strcmp(obj.devices.amplifier.inputLabels,'current'));
+                inunits = obj.devices.amplifier.inputUnits{ind(1)};
+                ylabel(findobj(obj.TrialDisplay,'tag','inputax'),inunits);
+            elseif sum(strcmp({'IClamp','IClamp_fast'},obj.devices.amplifier.mode))
+                invec = obj.inputs.data.voltage;
+                ind = find(strcmp(obj.devices.amplifier.inputLabels,'voltage'));
+                inunits = obj.devices.amplifier.inputUnits{ind(1)};
+                ylabel(findobj(obj.TrialDisplay,'tag','inputax'),inunits);
+            end
+                        
+            l = findobj(findobj(obj.TrialDisplay,'tag','inputax'),'tag','ampinput');
+            set(l,'ydata',invec);
+            
+            l = findobj(findobj(obj.TrialDisplay,'tag','outputax'),'tag','exposure');
+            set(l,'ydata',obj.inputs.data.exposure);
+
+        end
+
+    end
+    
+    methods (Access = protected)
+    end
+end
